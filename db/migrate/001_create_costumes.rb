f5891984326188d30c95dbfc15b9601f1ac7dde8
Harley Hanna <harleyhannawrit@gmail.com>
2020-09-21 11:39:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.string :name
       t.float :price
       t.string :size
-      t.string :imageurl
+      t.string :image_url
       t.datetime :created_at
       t.datetime :updated_at
     end
